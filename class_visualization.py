7997e8c105cd6ef64a92569dfef2229459e4a28f
Arjun Ashok <arjun2000ashok@gmail.com>
2020-08-28 13:42:50 +0000

    Create env for transfer

---
+++ class_visualization.py
@@ -0,0 +1,15 @@
+import torch
+import random
+import numpy as np
+from scipy.ndimage.filters import gaussian_filter1d
+import matplotlib.pyplot as plt
+from PIL import Image
+
+# fool train
+
+# return grids/images with 4 cols
+
+# main function that computes + passes it to return grids and gets the list of PILs and saves it
+
+# params: number of images, and iters and optionally those blur/print_every args output dir, 
+# filename: label + viz + label + random str.png
