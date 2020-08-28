96de5fbca12e18839b00e2d40a21c83e27a13b32
Arjun Ashok <arjun2000ashok@gmail.com>
2020-08-28 12:18:13 +0000

    Done with fooling, to transfer

---
+++ adversarial_fooling.py
@@ -0,0 +1,14 @@
+import torch
+import random
+import numpy as np
+from scipy.ndimage.filters import gaussian_filter1d
+import matplotlib.pyplot as plt
+from PIL import Image
+
+# fool train
+
+# return grids/images with 4 cols
+
+# main function that computes + passes it to return grids and gets the list of PILs and saves it
+
+# params: number of images, target y(or random), and output dir, filename: label + to + label + random str.png
--- saliency_maps.py
+++ saliency_maps.py
@@ -5,3 +5,10 @@ from scipy.ndimage.filters import gaussian_filter1d
 import matplotlib.pyplot as plt
 from PIL import Image
 
+# compute saliency
+
+# return grids/images with saliency maps
+
+# main function that computes + passes it to return grids and gets the list of PILs and saves it
+
+# params: number of images, and output dir, filename: label + random str.png
